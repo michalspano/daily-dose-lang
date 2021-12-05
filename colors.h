d5e85428de63f6491720ee6c770c49008f6ea9c3
michalspano <speko.spano@gmail.com>
2021-12-05 16:37:46 +0100

    Add colors header file

---
+++ colors.h
@@ -0,0 +1,11 @@
+// Include ANSI colors escape sequences
+char *RED = "\033[0;31m";
+char *GREEN = "\033[0;32m";
+char *YELLOW = "\033[0;33m";
+char *MAGENTA = "\033[0;35m";
+char *CYAN = "\033[0;36m";
+char *BLUE = "\033[0;34m";
+char *RESET = "\033[0m";
+char *ITALICS = "\033[3m";
+char *BOLD = "\033[1m";
+char* UNDERLINE = "\033[4m";
