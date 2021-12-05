542122d922b55b55b6f536091b08e790932e5e6d
michalspano <speko.spano@gmail.com>
2021-12-05 17:00:23 +0100

    Add sh scripts

---
+++ comp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Automated compilation script for the C/C++ compiler
+clang main.c -o ./bin/main -lm
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Automated script to run the program
+# FIXME: add multiple database options support
+./bin/main ./database/russian_wordlist.txt
