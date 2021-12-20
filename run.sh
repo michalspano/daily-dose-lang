312d901dc194ff82f0659050c2793361d2faf8cf
michalspano <speko.spano@gmail.com>
2021-12-20 22:58:05 +0100

    Revert the run script to run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,39 @@
 #!/bin/sh
-# Automated script to run the program
-# FIXME: add multiple database options support
-# FIXME: add global options support
-./bin/main ./database/russian_wordlist.txt
+# FIXME: Temporary replacement of run.sh
+current_path="$0"
+dict_lang="$1"
+
+# Strip to directory only
+current_path="${current_path%/*}"
+fixied_bin_path="/bin/main"
+
+database_path="${0%/*}/database/"
+
+# Load the name of the files from database_path to an array
+files=()
+
+for file in $database_path*; do
+    files+=("${file##*/}")
+done
+
+for f in "${files[@]}"; do
+    # Remove file extension
+    f="${f%.*}"
+    if [ "$f" = "${dict_lang}" ]; then
+        user_langugae_path="${database_path}${f}.txt"
+        echo "${user_langugae_path}"
+        break
+    fi
+    user_langugae_path="null"
+done
+
+# Concatinate the path and the binary name
+bin_path="$current_path$fixied_bin_path"
+
+# Default run
+if [ -z "$2" ]; then
+    exec "$bin_path" "$user_langugae_path"
+else
+    # Parse any additional flags
+    exec "$bin_path" "$user_langugae_path" "$2"
+fi  
